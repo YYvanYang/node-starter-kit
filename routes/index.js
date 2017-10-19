dcc274a58b346183a10d89070929ec2f7142ff26
Yvan Yang <yvan.yang@outlook.com>
2017-10-19 15:22:46 +0800

    add wechat share code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
+import Wechat from 'node-wechat-sign'
+
 const express = require('express');
 const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  let url = req.query.url
+  let signature = getSignature(url)
+  res.send('Hey! It works!', { wxConf: signature });
 });
+
+
+function getSignature() {
+  let appid = "todo"
+  let secret = "todo"
+  const config = config = {
+    appid,
+    secret,
+    tokenApiTemplate: "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{appid}&secret=#{secret}",
+    ticketApiTemplate: "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=#{access_token}&type=jsapi",
+    cacheExpiresInSeconds: 7000
+  }
+
+  const wechat = new Wechat(config)
+  const signature = await wechat.sign(url)
+  return signature
+}
 
 module.exports = router;
